5377d8c28fc46fdebd15f8632e7660863af1425a
ccropley <chad.cropley@gmail.com>
2020-05-12 17:49:19 -0700

    basic blog with example authentication completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+ http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+ 
+  def index
+    @articles = Article.all
+  end
+  
 end
